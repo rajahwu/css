6f4d3efc0d660514f910c462cdd0d46670ab4a80
rajahwu <vmradford@gmail.com>
2023-09-23 15:41:20 -0400

    add menu back btn svg

--- private/shared/header.php
+++ private/shared/header.php
@@ -11,7 +11,32 @@
   <div id="main">
     <div id="header">
       <div id="navigation">
-        <a id="nav-back-link" href=".."><img width="36" height="36" src="/apps/Training/css/public/assets/arrow_back_left_icon.svg" alt="back button icon"></a>
-        <a class="font-mono text-3xl font-bold text-yellow-300 normal-case btn btn-ghost" href="/apps/Training/css/public/">HOME</a>
+        <a id="nav-back-link" href="..">
+          <svg class="opacity[var(--tw-text-opacity)]" width="32px" height="32px" viewBox="0 0 24 24" id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" fill="#fde047" stroke="#fde047">
+            <g id="SVGRepo_bgCarrier" stroke-width="0" />
+            <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round" />
+            <g id="SVGRepo_iconCarrier">
+              <defs>
+                <style>
+                  .cls-1 {
+                    fill: none;
+                    stroke: #fde047;
+                    stroke-miterlimit: 10;
+                    stroke-width: 1.91px;
+                  }
+                </style>
+              </defs>
+              <circle class="cls-1" cx="12" cy="12" r="10.5" />
+              <polygon class="cls-1" points="9.14 12 15.82 8.18 15.82 15.82 9.14 12" />
+              <line class="cls-1" x1="8.18" y1="16.77" x2="8.18" y2="7.23" />
+            </g>
+          </svg>
+        </a>
+        <a class="font-mono text-3xl font-bold normal-case btn btn-ghost" href="/apps/Training/css/public/">
+          <svg enable-background="new 0 0 32 32" height="32px" id="Layer_1" version="1.1" viewBox="0 0 32 32" width="32px" xml:space="preserve" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+            <path d="M29.707,15.793l-13-13c-0.391-0.391-1.023-0.391-1.414,0l-13,13c-0.391,0.391-0.391,1.023,0,1.414s1.023,0.391,1.414,0L16,4.914l8.014,8.014C24.013,12.953,24,12.975,24,13v15H8V18c0-0.553-0.448-1-1-1s-1,0.447-1,1v11c0,0.553,0.448,1,1,1h18c0.553,0,1-0.447,1-1V14.914l2.293,2.293C28.488,17.402,28.744,17.5,29,17.5s0.512-0.098,0.707-0.293C30.098,16.816,30.098,16.184,29.707,15.793z" fill="white" stroke="rgb(253, 224, 71, var(--tw-text-opacity))" stroke-width="1" <!-- Adjust the stroke-width for a thin border -->
+              />
+          </svg>
+        </a>
       </div>
     </div>
--- training_modules/shared/training_card_css.php
+++ training_modules/shared/training_card_css.php
@@ -1,3 +1,3 @@
 <div class="w-5/6 p-2 mx-10 rounded card elevation-3" style="background-image: url(../../public/assets/images/bg-hero.jpg);">
     <div class="hero-overlay bg-opacity-60"></div>
-        <ul class="prose normal-case prose-h1:ml-5 prose-h1:text-yellow-300 prose-h2:text-yellow-200 prose-a:text-orange-500 prose-a:link">
+        <ul class="ml-5 prose rounded-lg prose-h1:ml-5 prose-h1:text-yellow-300 prose-h2:text-yellow-200 prose-a:text-orange-500 prose-a:link prose-a:capitalize prose-img:elevation-8 prose-img:rounded-xl">
