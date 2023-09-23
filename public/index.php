7edadbdf5c74694f69ff729646a796c9182467b3
rajahwu <vmradford@gmail.com>
2023-09-22 20:05:53 -0400

    add basic style to training pages

--- public/index.php
+++ public/index.php
@@ -2,15 +2,16 @@
 include_once('../private/initialize.php');
 include(SHARED_PATH . '/header.php');
 ?>
+<div class="prose">
+    <h1>CSS Traning</h1>
+    <a href="./public/links.php" target="_blank">Links</a>
 
-<h1>CSS Traning</h1>
-<a href="./public/links.php" target="_blank">Links</a>
+    <div>
+        <a href="../personal-site/" target="blank">Course Project Site</a>
+    </div>
 
-<div>
-    <a href="../personal-site/" target="blank">Course Project Site</a>
-</div>
-
-<div>
-    <a href="../training_modules/css_core_concepts/">css core concepts</a>
+    <div>
+        <a href="../training_modules/css_core_concepts/">css core concepts</a>
+    </div>
 </div>
 <?php include(SHARED_PATH . '/footer.php'); ?>
--- training_modules/css_core_concepts/index.php
+++ training_modules/css_core_concepts/index.php
@@ -1,6 +1,8 @@
 <?php
-include_once('../training_module_init.php');
-echo '<ul>';
-render_lesson_content("css core concept", "./note");
-echo '</ul>';
-include(SHARED_PATH . '/footer.php');
+include_once('../training_module_init.php'); ?>
+<ul class="prose">
+    <?php
+    render_lesson_content("css core concept", "./note");
+    echo '</ul>';
+    include(SHARED_PATH . '/footer.php');
+    ?>
